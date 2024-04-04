8a2d930fa8a4b8f5dc6e98d2db46ead3a71157d2
Thalyson Rian <Trungr@hotmail.com>
2024-04-04 19:44:42 -0300

    Fazendo função grep usando threads

--- main.py
+++ main.py
@@ -7,3 +7,19 @@ def initMap(arqv, padrao, results):
     results.append(resultMap)
 
 
+def initReduce(results):
+    resultReduce = Reduce(results)
+    for numLinha , linha in resultReduce:
+        print(f"{numLinha}--- {linha} ")
+
+def Grep(arqvs , padrao):
+    resultsMaps = []
+    threadsMap = []
+    for arq in arqvs:
+            thread = threading.Thread(target=initMap, args=(arq, resultsMaps))
+            threadsMap.append(thread)
+            thread.start()
+    for thread in threadsMap:
+         thread.join()
+
+    initReduce(resultsMaps)
